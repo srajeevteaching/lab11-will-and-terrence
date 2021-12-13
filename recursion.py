1bc474632519b86a9f0694a3febc582c7378a65b
WillGrimmer <90355797+WillGrimmer@users.noreply.github.com>
2021-12-13 00:05:28 -0500

    Delete recursion.py

--- recursion.py
+++
@@ -1,9 +0,0 @@
-def mystery( n ):
-   if n > 0:
-       print(n,"mystery function is called. ")
-      # print(n, end=" ")
-       mystery( n-1)
-       print( n, end = " " )
-   # else, do nothing
-# inside main:
-mystery( 5 )
